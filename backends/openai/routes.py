db7bf6ea4aa75465112932df6c7f825ebc2451d8
Gregory <gphorvath@defenseunicorns.com>
2024-02-15 19:00:56 -0500

    Modify embeddings endpoint to accept str and [str] (#63)
    * modify embeddings endpoint to accept str and [str]
    * test inputs to verify str and [str]

--- backends/openai/routes.py
+++ backends/openai/routes.py
@@ -107,9 +107,17 @@ async def embeddings(
             detail=f"Model {req.model} not found. Currently supported models are {list(model_config.models.keys())}",
         )
 
-    request = leapfrogai.EmbeddingRequest(inputs=[req.input])  # type: ignore
+    if type(req.input) is str:
+        request = leapfrogai.EmbeddingRequest(inputs=[req.input])
+    elif type(req.input) is list and all(isinstance(i, str) for i in req.input):
+        request = leapfrogai.EmbeddingRequest(inputs=req.input)
+    else:
+        raise HTTPException(
+            status_code=405,
+            detail=f"Invalid input type {type(req.input)}. Currently supported types are str and list[str]"
+        )
+        
     return await create_embeddings(model, request)
-
 
 @router.post("/audio/transcriptions")
 async def transcribe(
