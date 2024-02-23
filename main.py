249cd3313e697428a1debab0dbb9f1a5335fb0a3
John Alling <john.alling@defenseunicorns.com>
2024-02-23 10:00:03 -0500

    replace deprecated FastAPI decorators

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from fastapi import FastAPI
 # We need to import all the functions in these files so the router decorator gets processed
 from backends.openai import router as openai_router
 from backends.openai.routes import *
+from contextlib import asynccontextmanager
 from utils import get_model_config
 import asyncio
 
-app = FastAPI()
+
+# handle startup & shutdown tasks
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    print("Starting to watch for configs")
+    asyncio.create_task(get_model_config().watch_and_load_configs())
+    yield
+    # shutdown
+    print("Clearing model configs")
+    asyncio.create_task(get_model_config().clear_all_models())
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 # super simple healthz check
@@ -18,18 +32,6 @@ async def healthz():
 @app.get("/models")
 async def models():
     return get_model_config()
-
-
-@app.on_event("startup")
-async def watch_for_configs():
-    print("Starting to watch for configs")
-    asyncio.create_task(get_model_config().watch_and_load_configs())
-
-
-@app.on_event("shutdown")
-async def clear_configs():
-    print("Clearing model configs")
-    asyncio.create_task(get_model_config().clear_all_models())
 
 
 app.include_router(openai_router)
